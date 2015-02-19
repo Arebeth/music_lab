6b2e4caa0a03d3bbe74efbec23c8c80a8748814c
Arebeth Pease <arebethpease@gmail.com>
2015-02-18 22:13:25 -0800

    completed research, made basic sinatra app

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'  
+require 'pry'
+require "better_errors"
+require 'pg'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
+
+set :conn, PG.connect(dbname: 'sinatrasql')
+
+before do 
+  @conn = settings.conn
+end
+
+settings.conn
+
+get '/' do 
+  "Hello world"
+end
+  
